8b566310a5d047b0ce8cf28e7abd89e0dfe04270
Lukasz Kowalski <krowal@gmail.com>
2015-10-28 16:27:24 +0100

    added main loop, requestAnimationFrame support

--- main.js
+++ main.js
@@ -18,12 +18,13 @@
 
         this.init = function(){
             ctx = _context.DOM.canvas.getContext('2d');
-            this.draw();
         };
     })(this);
 
     var Game = new (function(_context)
     {
+        var spinning = false;
+
         this.onChoiceChange = function(e)
         {
             var val = e.target.value;
@@ -33,7 +34,24 @@
         this.onSpinClick = function()
         {
             //TODO:start spin
-            _context.DOM.spinButton.classList.add('disabled');
+            if(!spinning)
+            {
+                _context.DOM.spinButton.classList.add('disabled');
+                _context.DOM.choice.select.disabled = true;
+                spinning = true;
+
+                this.startSpin();
+            }
+        };
+
+        this.startSpin = function()
+        {
+
+        };
+
+        this.endSpin = function()
+        {
+
         };
 
         this.symbolPreview = function(symbol)
@@ -47,13 +65,46 @@
             img.classList.add("pop-in");
         };
 
+        this.getCurrentChoice = function()
+        {
+            return _context.symbols[_context.DOM.choice.select.value];
+        };
+
+        this.update = function(dt)
+        {
+
+            Renderer.draw();
+        };
+
+        var then = new Date(),
+            now = new Date(),
+            delta;
+
+        this.tick = function(){
+            requestAnimationFrame(this.tick);
+
+            delta = now.getTime() - then.getTime();
+            then = now;
+            now = new Date();
+
+            this.update();
+        };
+
         this.start = function()
         {
             this.symbolPreview(_context.DOM.choice.select.value);
             Renderer.init();
+
+            this.tick();
         };
     })(this);
 
+
+    /**
+     * Initializing html elements events listeners,
+     * loading config json and images
+     * and starting the game engine
+     */
     this.init = function()
     {
         this.DOM = {
@@ -147,7 +198,7 @@
     };
 
     /**
-     * Initializing everything only
+     * Starting initialisation process
      * when whole document is ready
      */
     if(document.readyState === "complete")
